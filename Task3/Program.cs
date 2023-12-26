a838b7e8423dd9ddebe8333261189aa1d9fa6c96
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-26 17:24:16 +0400

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+int intDigit = new Random().Next(10,100);
+System.Console.WriteLine(intDigit);
+int firstNum = intDigit/10;
+int secondNum = intDigit%10;
+if(firstNum > secondNum)
+{
+    System.Console.WriteLine($"from integer digits - {intDigit}- the bigger is {firstNum}");
+}
+else           //if firstNum == secondNum it also will be wrigth answer with else.
+{
+    System.Console.WriteLine($"from integer digits - {intDigit}- the bigger is {secondNum}");
+}
+
