559fe2cc263327a6adef9c29a24258f4180e4de5
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-26 22:31:14 +0400

    Task4VS

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+System.Console.WriteLine("Enter the number");
+int N = Convert.ToInt32(Console.ReadLine());
+int countOfNum = 0;
+long baseN = N/10;
+if(baseN==0)
+{
+countOfNum = 1;
+
+}
+else
+{
+    while(baseN!=0)
+    {
+        baseN = baseN/10;
+        countOfNum = countOfNum +1;
+    }
+    
+}
+
+int[] numbersOfN = new int[countOfNum+1];
+for(int i = 0;i<=countOfNum ; i++ )
+{
+numbersOfN[i] = N % 10;
+N = N/10;
+Console.Write(numbersOfN[i] +",");
+}
+
+
+
+
+
