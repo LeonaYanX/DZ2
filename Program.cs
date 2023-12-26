6389165ab01b25a8fdae46925e89dd48c5f3ec92
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2023-12-26 16:19:12 +0400

    Task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1 
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+System.Console.WriteLine("Enter the value");
+int value = Convert.ToInt32(Console.ReadLine());
+if(value%7==0 && value%23 == 0 )
+{
+    if(value != 0)
+    {
+    System.Console.WriteLine("Yes");
+    }
+    else
+    {
+        System.Console.WriteLine("NONONO it's zero");
+    }
+}
+else
+{
+System.Console.WriteLine("No");
+}
